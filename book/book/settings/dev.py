36b07e61e33107b0804c4ade846f0917dea453bc
github-python-yang <522996297@qq.com>
2019-11-16 19:37:51 +0800

    配置数据库mysql

--- book/book/settings/dev.py
+++ book/book/settings/dev.py
@@ -76,10 +76,18 @@ WSGI_APPLICATION = 'book.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+    "default": {
+        'ENGINE': 'django.db.backends.mysql', # 数据库引擎
+        'HOST': '127.0.0.1', # 数据库主机
+        'PORT': 3306, # 数据库端口
+        'USER': 'Eben', # 数据库用户名
+        'PASSWORD': '123456', # 数据库用户密码
+        'NAME': 'books' # 数据库名字
+    },
 }
 
 
